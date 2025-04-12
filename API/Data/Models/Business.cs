6c15d26b8db58965e1d6cdfa9efacc9968b99175
Ntwisiso <amagames.br@gmail.com>
2025-04-12 16:00:40 +0200

    Adding Controllers

--- API/Data/Models/Business.cs
+++ API/Data/Models/Business.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewEngines;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API.Data.Models
 {
     public class Business
     {
+        [Key]
         [Required]
         public Guid Id { get; set; } = Guid.NewGuid();
 
@@ -25,6 +25,10 @@ namespace API.Data.Models
         public bool IsVerified { get; set; } = false;
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign Key
+        [Required]
+        public Guid UserId { get; set; }
 
         // Navigation Properties
         [ForeignKey("UserId")]
