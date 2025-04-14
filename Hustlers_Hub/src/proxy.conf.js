314d0755b69617c01246b0a3bad169a4e6381a78
Ntwisiso <amagames.br@gmail.com>
2025-04-14 17:57:50 +0200

    Update Ports

--- Hustlers_Hub/src/proxy.conf.js
+++ Hustlers_Hub/src/proxy.conf.js
@@ -1,14 +1,14 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:70184';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7018';
 
 const PROXY_CONFIG = [
   {
     context: [
       "/api",
     ],
-    target: "http://localhost:50016",
+    target: "http://localhost:40018",
     secure: false
   }
 ]
