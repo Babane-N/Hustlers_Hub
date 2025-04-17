a4b06a7c82946d262e1fff7356cade0c4d1ea763
Ntwisiso <amagames.br@gmail.com>
2025-04-17 16:06:44 +0200

    Adding components

--- Hustlers_Hub/src/app/app.module.ts
+++ Hustlers_Hub/src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookingsComponent } from './bookings/bookings.component';
+import { SearchComponent } from './search/search.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    BookingsComponent,
+    SearchComponent,
+    AdminComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
