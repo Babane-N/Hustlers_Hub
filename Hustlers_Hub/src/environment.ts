5a6cf4946219582c65e4ea3ab2aa4c8b8be9a71a
Ntwisiso Babane <babane.nq@gmail.com>
2025-10-27 08:16:01 +0200

    environment production update

--- Hustlers_Hub/src/environment.ts
+++ Hustlers_Hub/src/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://hustlershub-g3cjffaea3axckg3.southafricanorth-01.azurewebsites.net/api'
+  production: false,
+  apiUrl: 'https://localhost:7018/api'
 };
