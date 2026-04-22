b2f4e6e6e31adfd3705b7d55d96ff7c53c4c6700
Ntwisiso Babane <babane.nq@gmail.com>
2026-04-22 16:20:27 +0200

    Android App

--- Hustlers_Hub/android/app/src/main/java/com/example/app/MainActivity.java
+++ Hustlers_Hub/android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,24 @@
 package com.example.app;
 
+import android.os.Bundle;
+import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+
+    // Install splash screen BEFORE super.onCreate()
+    SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
+    super.onCreate(savedInstanceState);
+
+    // Capacitor automatically loads your web app (no need for setContentView)
+    // Optional: keep splash while loading
+    splashScreen.setKeepOnScreenCondition(() -> {
+      // Return true to keep splash, false to dismiss
+      return false;
+    });
+  }
+}
--- Hustlers_Hub/src/app/promotion/promotion.component.ts
+++ Hustlers_Hub/src/app/promotion/promotion.component.ts
@@ -24,8 +24,8 @@ export class PromotionComponent implements OnInit {
   selectedCategory: string = '';
   sortOrder: string = '';
   isLoading: boolean = false;
-  readonly apiUrl = 'https://hustlershub-g3cjffaea3axckg3.southafricanorth-01.azurewebsites.net/api/promotions';
-
+  //readonly apiUrl = 'https://hustlershub-g3cjffaea3axckg3.southafricanorth-01.azurewebsites.net/api/promotions';
+  readonly apiUrl = 'https://localhost:7018';
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
