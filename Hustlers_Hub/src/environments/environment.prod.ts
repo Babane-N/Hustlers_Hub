9e769f557b9094aa180cd46a4847968a00eee8c2
Ntwisiso Babane <babane.nq@gmail.com>
2026-04-24 19:49:37 +0200

    Updating Environment

--- Hustlers_Hub/src/environments/environment.prod.ts
+++ Hustlers_Hub/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hustlershub-g3cjffaea3axckg3.southafricanorth-01.azurewebsites.net/api',
-  uploadsUrl: 'https://hustlershub-g3cjffaea3axckg3.southafricanorth-01.azurewebsites.net/uploads'
+  apiUrl: 'https://hustlerhub-cea4bhbjdrgfdefb.southafricanorth-01.azurewebsites.net/api',
+  uploadsUrl: 'https://hustlerhub-cea4bhbjdrgfdefb.southafricanorth-01.azurewebsites.net.net/uploads'
 };
