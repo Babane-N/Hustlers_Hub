1a1c0a850122879abe90cc261d334ab67b432072
Ntwisiso Babane <babane.nq@gmail.com>
2026-05-05 20:10:15 +0200

    Trying to fix connection string

--- API/Program.cs
+++ API/Program.cs
@@ -45,7 +45,7 @@ builder.Services.AddScoped<IEmailService, EmailService>();
 // ---------------------------
 var allowedOrigins = new[]
 {
-    "https://agreeable-grass-0e90e7a03.7.azurestaticapps.net/",
+    "https://agreeable-grass-0e90e7a03.7.azurestaticapps.net",
     "https://hustlerhub-cea4bhbjdrgfdefb.southafricanorth-01.azurewebsites.net",
     "https://localhost:4200",
     "http://localhost:4200"
@@ -84,6 +84,7 @@ if (!Directory.Exists(uploadsPath))
 
 
 
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
