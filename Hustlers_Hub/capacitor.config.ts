9e0b5da06c900d4ff183e14900bec1aeae57d604
Ntwisiso Babane <babane.nq@gmail.com>
2026-06-21 23:36:29 +0200

    com.hustler

--- Hustlers_Hub/capacitor.config.ts
+++ Hustlers_Hub/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.hustle.app',
   appName: 'hustlers-hub',
   webDir: 'dist/hustlers-hub'
 };
